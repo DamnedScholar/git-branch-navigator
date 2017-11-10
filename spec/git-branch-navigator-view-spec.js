9f68197d407a2e863ce6ce31d5df234b6cacbddb
DamnedScholar <stickfigureonfire@gmail.com>
2017-11-09 19:21:07 -0600

    Initial commit.

---
+++ spec/git-branch-navigator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitBranchNavigatorView from '../lib/git-branch-navigator-view';
+
+describe('GitBranchNavigatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
